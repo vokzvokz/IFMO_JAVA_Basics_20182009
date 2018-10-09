7a173caaaf61100911bbf02ec839daee487cfdbd
Владимир <vokz@bk.ru>
2018-10-10 01:38:35 +0300

    All tests passed

--- src/main/java/ru/ifmo/cet/javabasics/BottleSong.java
+++ src/main/java/ru/ifmo/cet/javabasics/BottleSong.java
@@ -32,12 +32,46 @@ package ru.ifmo.cet.javabasics;
  */
 public class BottleSong {
 
+    private int bottleTakenAtOnce;
+    private static String[] ones = {"", " one", " two", " three", " four", " five", " six",
+            " seven", " eight", " nine"};
+    private static String[] teens = {" eleven", " twelve", " thirteen", " fourteen", " fifteen",
+            " sixteen", " seventeen", " eighteen", " nineteen"};
+    private static String[] tens = {"", " ten", " twenty", " thirty", " forty", " fifty", " sixty",
+            " seventy", " eighty", " ninety"};
+
     public BottleSong(int bottleTakenAtOnce) {
-        //TODO
+        this.bottleTakenAtOnce = bottleTakenAtOnce;
     }
 
+    private static String intToWords(int a){
+        if(a>10 && a <20) return teens[a-11];
+        else{
+            return tens[a/10]+ones[a%10];
+        }
+    }
+
+    private static String numberOfBottles(int a){
+        return Integer.toString(a)+((a==1)?" bottle":" bottles");
+    }
     public String getBottleSongLyrics() {
-        //TODO
-        throw new UnsupportedOperationException();
+        if(this.bottleTakenAtOnce>99 || this.bottleTakenAtOnce<1)
+            throw new java.lang.IllegalArgumentException();
+        int bottles = 99;
+        String lyrics = "";
+        while(bottles > this.bottleTakenAtOnce){
+            lyrics = lyrics + numberOfBottles(bottles)+" of beer on the wall, "+numberOfBottles(bottles)+" of beer."
+                    +"\nTake"+intToWords(this.bottleTakenAtOnce)+" down and pass around, "
+                    +numberOfBottles(bottles-bottleTakenAtOnce)+" of beer on the wall.\n";
+            bottles-=this.bottleTakenAtOnce;
+        }
+        lyrics = lyrics + numberOfBottles(bottles)+" of beer on the wall, "+numberOfBottles(bottles)+" of beer."
+                +"\nTake"+intToWords(bottles)+" down and pass around," +
+                " no more bottles of beer on the wall.\n" +
+                "No more bottles of beer on the wall, no more bottles of beer.\n" +
+                "Go to the store and buy some more, 99 bottles of beer on the wall.\n";
+        //
+        return lyrics;
+
     }
 }
